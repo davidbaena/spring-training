e1bc6616a8ba39a0975dee0c324f7434f01a16ad
david.baena <david.baena@bq.com>
2019-09-14 14:48:27 +0200

    Project setup

---
+++ src/main/kotlin/com/davidbaena/springtraining/HelloController.kt
@@ -0,0 +1,12 @@
+package com.davidbaena.springtraining
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @RequestMapping("/")
+    fun index(): String {
+        return "Greetings from Spring Boot!"
+    }
+}
